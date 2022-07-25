95bbf2abc7c9c01a6ce0c0ef7947b0545747d7b8
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-07-25 06:14:09 +0200

    Update set_from_map.go

--- set_from_map.go
+++ set_from_map.go
@@ -4,6 +4,11 @@ import (
   "fmt"
 )
 
-func main {
-  fmt.Println(".") 
+func main () {
+  set := map[int]bool{}
+  values := []int{9, 3, 7, 7, 2, 6, 9, 1, 3, 4, 8, 1, 5, 4}
+  for _, value := range(values) {
+    set[value] = true
+  }
+  fmt.Println(set) 
 }
